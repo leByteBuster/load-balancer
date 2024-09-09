c1142f99235aed1fe61bb0071eb03dd9dcfbe769
Axel Schuster <axel_schuster_@gmx.net>
2024-09-09 16:21:53 +0200

    Task 1.1: Simple webserver handling a get request
    Server serves on localhost port 8080 and returns a string "Connection
    was successful" on path "/"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use log::{debug, info};
+use std::env;
+use std::error::Error;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Read in env vars
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()?;
+
+    let addr: SocketAddr = format!("{}:{}", host, port).parse()?;
+
+    let tcp = TcpListener::bind(&addr).await?;
+
+    let router = Router::new().route("/", get(handle_request));
+
+    println!("Server is running on {}:{}", host, port);
+
+    let server_handle = axum::serve(tcp, router).await;
+
+    if let Err(e) = server_handle {
+        eprintln!("Server error: {}", e);
+    }
+
+    Ok(())
+}
+
+async fn handle_request() -> &'static str {
+    info!("Client Connected"); // server log
+    "Connection was successful" // answer for client
 }
